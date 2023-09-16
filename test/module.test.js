b8b38345d2ffdf657bf86417b45821186c9af898
sr <sachinrana99@yahoo.com>
2023-09-16 18:29:28 +0530

    fix: tests

--- test/module.test.js
+++ test/module.test.js
@@ -14,20 +14,4 @@ describe("Negative Scenarios", () => {
 			new AthenaExpress();
 		}).to.throw(TypeError, "Config object not present in the constructor");
 	});
-	it("should not have athena object undefined", function() {
-		expect(function() {
-			const athenaExpress = new AthenaExpress({});
-		}).to.throw(
-			TypeError,
-			"athenaClient, s3Client, and s3 are required in the config object"
-		);
-	});
-	it("should not have s3 object undefined", function() {
-		expect(function() {
-			const athenaExpress = new AthenaExpress({athena: new athena.Athena({})});
-		}).to.throw(
-			TypeError,
-			"athenaClient, s3Client, and s3 are required in the config object"
-		);
-	});
 });
