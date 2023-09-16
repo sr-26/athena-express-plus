ee1357203c8c4422d08ad155068479f0d73bf9e8
sr <sachinrana99@yahoo.com>
2023-09-16 18:30:06 +0530

    fix: tests

--- test/module.test.js
+++ test/module.test.js
@@ -2,9 +2,7 @@
 
 const chai = require("chai"),
 	expect = chai.expect,
-	AthenaExpress = require(".."),
-	athena = require("@aws-sdk/client-athena"),
-	s3 = require("@aws-sdk/client-s3");
+	AthenaExpress = require("..")
 
 chai.should();
 
