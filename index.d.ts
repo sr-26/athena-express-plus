f7e36e0ad42df3502b146bc00eb7b3887da0be4d
bruno <contato.bduaart@gmail.com>
2024-03-27 16:08:51 -0300

    replace client to client config

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 declare module 'athena-express-plus' {
-    import { S3Client } from '@aws-sdk/client-s3';
-    import { AthenaClient } from '@aws-sdk/client-athena';
+    import { S3ClientConfig } from '@aws-sdk/client-s3';
+    import { AthenaClientConfig } from '@aws-sdk/client-athena';
     interface ConnectionConfigInterface {
-        s3: S3Client;
-        athena: AthenaClient;
+        s3: S3ClientConfig;
+        athena: AthenaClientConfig;
         s3Bucket: string;
         getStats: boolean;
         db: string,
